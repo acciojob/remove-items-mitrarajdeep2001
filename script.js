cdf7d90fb7937ce6bc5e4e60844a2170a1362c64
Rajdeep Mitra <116889292+mitrarajdeep2001@users.noreply.github.com>
2023-04-17 13:02:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let colorSelect = document.querySelector('#colorSelect')
+let button = document.querySelector('input')
+button.addEventListener('click', () => {
+	let selectedOption = colorSelect.options[colorSelect.selectedIndex]
+	colorSelect.removeChild(selectedOption)
+})
